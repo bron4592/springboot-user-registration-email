6f29d41a350f9af84207db751fbc9ff61b36d156
Brandon ONeill <bron4592@colorado.edu>
2021-05-09 14:09:09 -0800

    added confirmation token database

--- src/main/java/com/brandon/demo/registration/RegistrationService.java
+++ src/main/java/com/brandon/demo/registration/RegistrationService.java
@@ -1,10 +1,33 @@
 package com.brandon.demo.registration;
 
+import com.brandon.demo.appuser.AppUser;
+import com.brandon.demo.appuser.AppUserRole;
+import com.brandon.demo.appuser.AppUserService;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@AllArgsConstructor
 public class RegistrationService {
+
+    private final AppUserService appUserService;
+    private final EmailValidator emailValidator;
+
     public String register(RegistrationRequest request) {
-        return "works";
+        boolean isValidEmail = emailValidator.test(request.getEmail());
+
+        if (!isValidEmail){
+            throw new IllegalStateException("Email not valid");
+        }
+
+        return appUserService.signUpUser(
+                new AppUser(
+                        request.getFirstName(),
+                        request.getLastName(),
+                        request.getEmail(),
+                        request.getPassword(),
+                        AppUserRole.USER
+                )
+        );
     }
 }
