a9d1f158a88200710b8cb5dd73669305a20d7392
Brandon ONeill <bron4592@colorado.edu>
2021-05-08 21:52:40 -0800

    first commit

---
+++ src/main/java/com/brandon/demo/UserRegistrationEmailApplication.java
@@ -0,0 +1,13 @@
+package com.brandon.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserRegistrationEmailApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserRegistrationEmailApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/brandon/demo/UserRegistrationEmailApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brandon.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserRegistrationEmailApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
