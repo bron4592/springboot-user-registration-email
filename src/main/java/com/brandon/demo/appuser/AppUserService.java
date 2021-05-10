93bfe6fa7ef00f217ba344bcbcf747e10def865e
Brandon ONeill <bron4592@colorado.edu>
2021-05-09 16:19:09 -0800

    added email confirmation service

--- src/main/java/com/brandon/demo/appuser/AppUserService.java
+++ src/main/java/com/brandon/demo/appuser/AppUserService.java
@@ -30,25 +30,32 @@ public class AppUserService implements UserDetailsService {
     public String signUpUser(AppUser appUser){
         boolean userExists = appUserRepository.findByEmail(appUser.getEmail()).isPresent();
 
-        if (userExists){
+        if (userExists && appUser.getEnabled()){
             throw new IllegalStateException("email already taken");
         }
-        String encodedPassword = bCryptPasswordEncoder.encode(appUser.getPassword());
-        appUser.setPassword(encodedPassword);
+        else if (userExists && !appUser.getEnabled()){
+            // TODO: Implement new token for the user
+            String message = "Resending Confirmation Email";
+            return message;
+        }
+        else {
+            String encodedPassword = bCryptPasswordEncoder.encode(appUser.getPassword());
+            appUser.setPassword(encodedPassword);
 
-        appUserRepository.save(appUser);
+            appUserRepository.save(appUser);
 
-        String token = UUID.randomUUID().toString();
-        ConfirmationToken confirmationToken = new ConfirmationToken(
-                token,
-                LocalDateTime.now(),
-                LocalDateTime.now().plusMinutes(15),
-                appUser
-        );
-        confirmationTokenService.saveConfirmationToken(confirmationToken);
 
-        // TODO: Send email
-        return token;
+            String token = UUID.randomUUID().toString();
+            ConfirmationToken confirmationToken = new ConfirmationToken(
+                    token,
+                    LocalDateTime.now(),
+                    LocalDateTime.now().plusMinutes(15),
+                    appUser
+            );
+            confirmationTokenService.saveConfirmationToken(confirmationToken);
+
+            return token;
+        }
     }
 
     public int enableAppUser(String email) {
